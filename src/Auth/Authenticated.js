76456851cb226560831a7c886593d252e2669095
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-11 17:47:35 -0500

    Add Modify, Create and Delete Cupon

--- src/Auth/Authenticated.js
+++ src/Auth/Authenticated.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "../components/Navbar";
 import Admins from "../pages/admins";
 import Cupons from "../pages/cupons";
+import CreateCupon from "../pages/Cupons/createCupon";
+import DeleteCupon from "../pages/Cupons/deleteCupon";
+import ModifyCupon from "../pages/Cupons/modifyCupon";
 import CreateEmployee from "../pages/Empleado/createEmployee";
 import EditEmployee from "../pages/Empleado/editEmloyee";
 import Empleados from "../pages/employees";
@@ -20,6 +23,9 @@ function Authenticated() {
         <Route path="/navbar" element={<NavBar />} />
         <Route path="/clientes" element={<h2>Clientes</h2>} />
         <Route path="/cupones" element={<Cupons/>} />
+        <Route path="/cupones/create_cupon" element={<CreateCupon/>} />
+        <Route path="/cupones/modify_cupon" element={<ModifyCupon/>} />
+        <Route path="/cupones/delete_cupon" element={<DeleteCupon/>} />
         <Route path="/reportes" element={<h2>Reportes y eventualidades</h2>} />
         <Route path="/empleados" element={<Empleados/>} />
         <Route path="/empleados/edit" element={<EditEmployee/>} />
