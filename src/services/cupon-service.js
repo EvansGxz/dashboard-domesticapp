9ecbbcb58dd566ea07e3dc4f82ff25e78fd43480
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-11 17:48:30 -0500

    Add cupon services

--- src/services/cupon-service.js
+++ src/services/cupon-service.js
@@ -1,5 +1,25 @@
 import apiFetch from "./api-fetch";
 
 export async function showCupon(CuponrID) {
-  return await apiFetch(`cupons/${CuponrID}`);
+  return await apiFetch(`cupon/${CuponrID}`);
+}
+
+export async function indexCupon() {
+  return await apiFetch(`cupons/`);
+}
+
+export async function createCupon(newCupon) {
+  return await apiFetch('cupons/', { body: newCupon });
+   
+}
+
+export async function updateCupon(data) {
+  return await apiFetch("cupons/", {
+    body: data,
+    method: "PATCH",
+  });
+}
+
+export async function deleteCupon(CuponUserID) {
+  return await apiFetch(`cupons/${CuponUserID}`, { method: "DELETE" });
 }
