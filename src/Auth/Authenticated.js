6f93e0f87f499cf975e254cf6376a0d0e879a87e
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-10 21:22:51 -0500

    Add Routes

--- src/Auth/Authenticated.js
+++ src/Auth/Authenticated.js
@@ -8,6 +8,18 @@ function Authenticated() {
   return (
     <>
       <Routes>
+      <Route path="/" element={<h2>Calendar</h2>} />
+      <Route path="/*" element={<h2>Calendar</h2>} />
+      <Route path="/calendar" element={<h2>Calendar</h2>} />
+
+      <Route path="/clientes" element={<h2>Clientes</h2>} />
+      <Route path="/cupones" element={<h2>Cupones</h2>} />
+      <Route path="/reportes" element={<h2>Reportes y eventualidades</h2>} />
+      <Route path="/empleados" element={<h2>Empleados</h2>} />
+      <Route path="/soporte" element={<h2>Chat de Soporte</h2>} />
+      <Route path="/estadisica" element={<h2>Estadisticas</h2>} />
+      <Route path="/capacitaciones" element={<h2>Capacitaciones</h2>} />
+      <Route path="/gestion" element={<h2>Gestion Avanzada</h2>} />
       </Routes>
     </>
   );
