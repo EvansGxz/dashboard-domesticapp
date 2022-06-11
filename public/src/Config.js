524b9f3faa6ea97b758d8ba480f6520c016811e4
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-10 22:46:37 -0500

    Pages

--- public/src/Config.js
+++
@@ -1,4 +0,0 @@
-
-export const BASE_URI ="https://domesticapp-api.herokuapp.com/";
-export const tokenKey = "Domesticapp"
-
--- src/Auth/Authenticated.js
+++ src/Auth/Authenticated.js
@@ -1,25 +1,32 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import { useAuth } from "../context/auth-context";
-import { Navigate } from "react-router-dom";
+import Admins from "../pages/admins";
+import Cupons from "../pages/cupons";
+import CreateEmployee from "../pages/Empleado/createEmployee";
+import EditEmployee from "../pages/Empleado/editEmloyee";
+import Empleados from "../pages/employees";
+import Stats from "../pages/estadistica";
+import Servicios from "../pages/servicios";
 
 function Authenticated() {
-  const { user } = useAuth();
   return (
     <>
       <Routes>
-      <Route path="/" element={<h2>Calendar</h2>} />
-      <Route path="/*" element={<h2>Calendar</h2>} />
-      <Route path="/calendar" element={<h2>Calendar</h2>} />
+        <Route path="/" element={<h2>Calendar</h2>} />
+        <Route path="/*" element={<h2>Calendar</h2>} />
+        <Route path="/calendar" element={<h2>Calendar</h2>} />
 
-      <Route path="/clientes" element={<h2>Clientes</h2>} />
-      <Route path="/cupones" element={<h2>Cupones</h2>} />
-      <Route path="/reportes" element={<h2>Reportes y eventualidades</h2>} />
-      <Route path="/empleados" element={<h2>Empleados</h2>} />
-      <Route path="/soporte" element={<h2>Chat de Soporte</h2>} />
-      <Route path="/estadisica" element={<h2>Estadisticas</h2>} />
-      <Route path="/capacitaciones" element={<h2>Capacitaciones</h2>} />
-      <Route path="/gestion" element={<h2>Gestion Avanzada</h2>} />
+        <Route path="/clientes" element={<h2>Clientes</h2>} />
+        <Route path="/cupones" element={<Cupons/>} />
+        <Route path="/reportes" element={<h2>Reportes y eventualidades</h2>} />
+        <Route path="/empleados" element={<Empleados/>} />
+        <Route path="/empleados/edit" element={<EditEmployee/>} />
+        <Route path="/empleados/create" element={<CreateEmployee/>} />
+        <Route path="/soporte" element={<h2>Chat de Soporte</h2>} />
+        <Route path="/estadisica" element={<Stats/>} />
+        <Route path="/capacitaciones" element={<h2>Capacitaciones</h2>} />
+        <Route path="/gestion" element={<Admins />} />
+        <Route path="/servicios" element={<Servicios />} />
       </Routes>
     </>
   );
