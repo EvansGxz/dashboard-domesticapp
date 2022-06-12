0d7760706f4509530f819cfcf6406d50ca94a55d
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-12 15:05:04 -0500

    Add customer page

--- src/Auth/Authenticated.js
+++ src/Auth/Authenticated.js
@@ -1,16 +1,14 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "../components/Navbar";
-import Admins from "../pages/admins";
+import Clientes from "../pages/Clientes";
 import Cupons from "../pages/cupons";
 import CreateCupon from "../pages/Cupons/createCupon";
 import DeleteCupon from "../pages/Cupons/deleteCupon";
 import ModifyCupon from "../pages/Cupons/modifyCupon";
 import CreateEmployee from "../pages/Empleado/createEmployee";
 import EditEmployee from "../pages/Empleado/editEmloyee";
-import Empleados from "../pages/employees";
-import Stats from "../pages/estadistica";
-import Servicios from "../pages/servicios";
+import Empleados from "../pages/Empleados";
+
 
 function Authenticated() {
   return (
@@ -20,8 +18,8 @@ function Authenticated() {
         <Route path="/*" element={<h2>Calendar</h2>} />
         <Route path="/calendar" element={<h2>Calendar</h2>} />
 
-        <Route path="/navbar" element={<NavBar />} />
-        <Route path="/clientes" element={<h2>Clientes</h2>} />
+        <Route path="/navbar" element={<h2>Calendar</h2>} />
+        <Route path="/clientes" element={<Clientes/>} />
         <Route path="/cupones" element={<Cupons/>} />
         <Route path="/cupones/create_cupon" element={<CreateCupon/>} />
         <Route path="/cupones/modify_cupon" element={<ModifyCupon/>} />
@@ -31,10 +29,10 @@ function Authenticated() {
         <Route path="/empleados/edit" element={<EditEmployee/>} />
         <Route path="/empleados/create" element={<CreateEmployee/>} />
         <Route path="/soporte" element={<h2>Chat de Soporte</h2>} />
-        <Route path="/estadisica" element={<Stats/>} />
+        <Route path="/estadisica" element={<h2>Calendar</h2>} />
         <Route path="/capacitaciones" element={<h2>Capacitaciones</h2>} />
-        <Route path="/gestion" element={<Admins />} />
-        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/gestion" element={<h2>Calendar</h2>} />
+        <Route path="/servicios" element={<h2>Calendar</h2>} />
       </Routes>
     </>
   );
