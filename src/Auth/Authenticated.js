d6a54c22271199c2803447d39fd433c3ecda1aae
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-12 20:24:22 -0500

    Add services routes

--- src/Auth/Authenticated.js
+++ src/Auth/Authenticated.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Calendario } from "../pages/Calendario";
 import Clientes from "../pages/Clientes";
 import CrearCliente from "../pages/Clientes/createCliente";
 import EditarCliente from "../pages/Clientes/editCliente";
@@ -10,17 +11,19 @@ import ModifyCupon from "../pages/Cupons/modifyCupon";
 import CreateEmployee from "../pages/Empleado/createEmployee";
 import EditEmployee from "../pages/Empleado/editEmloyee";
 import Empleados from "../pages/Empleados";
+import CrearServicio from "../pages/Servicios/crearServicio";
+import EditarServicio from "../pages/Servicios/editarServicio";
 
 
 function Authenticated() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h2>Calendar</h2>} />
-        <Route path="/*" element={<h2>Calendar</h2>} />
-        <Route path="/calendar" element={<h2>Calendar</h2>} />
+        <Route path="/" element={<Calendario/>} />
+        <Route path="/*" element={<Calendario/>} />
+        <Route path="/calendar" element={<Calendario/>} />
 
-        <Route path="/navbar" element={<h2>Calendar</h2>} />
+        <Route path="/navbar" element={<Calendario/>} />
         <Route path="/clientes" element={<Clientes/>} />
         <Route path="/clientes/edit" element={<EditarCliente/>} />
         <Route path="/clientes/create" element={<CrearCliente/>} />
@@ -33,10 +36,12 @@ function Authenticated() {
         <Route path="/empleados/edit" element={<EditEmployee/>} />
         <Route path="/empleados/create" element={<CreateEmployee/>} />
         <Route path="/soporte" element={<h2>Chat de Soporte</h2>} />
-        <Route path="/estadisica" element={<h2>Calendar</h2>} />
+        <Route path="/estadisica" element={<Calendario/>} />
         <Route path="/capacitaciones" element={<h2>Capacitaciones</h2>} />
-        <Route path="/gestion" element={<h2>Calendar</h2>} />
-        <Route path="/servicios" element={<h2>Calendar</h2>} />
+        <Route path="/gestion" element={<Calendario/>} />
+        <Route path="/servicios" element={<Calendario/>} />
+        <Route path="/servicios/create" element={<CrearServicio/>} />
+        <Route path="/servicios/edit" element={<EditarServicio/>} />
       </Routes>
     </>
   );
