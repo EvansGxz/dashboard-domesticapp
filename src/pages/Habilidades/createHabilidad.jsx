f54989a2a2d6540ad90c0c1d27896a3eb86c6ff2
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-18 11:59:58 -0500

    habds

--- src/pages/Habilidades/createHabilidad.jsx
+++ src/pages/Habilidades/createHabilidad.jsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { createHability, indexHability } from "../../services/habilities-services";
+import { BASE_URI } from "../../Config";
+import { indexHability } from "../../services/habilities-services";
 import { Input } from "../../styles/views/Login";
 
 const StyledForm = styled.form`
@@ -18,16 +18,28 @@ export default function CrearHabilidad() {
   const [form, setForm] = useState({
     hability: "",
     body: "",
+    image: "",
   });
-  const navigate = useNavigate();
 
   function handleSubmit(event) {
     event.preventDefault();
-    createHability(form).then(() => {
-      indexHability().then(setHabilidades);
-      navigate("/habilidades")
-    });
+
+    const data = new FormData();
+
+    data.append("hability", event.target.hability.value);
+    data.append("body", event.target.body.value);
+    data.append("image", event.target.image.files[0]);
+
+    submitAPI(data)
   }
+
+  function submitAPI(data) {
+    fetch(BASE_URI+`hability`,{
+    method: "POST",
+    body: data
+  }).then(response => response.json())
+}
+
 
   function handleFormChange(event) {
     const { name, value } = event.target;
@@ -38,7 +50,7 @@ export default function CrearHabilidad() {
     <ContainerAll>
     
     {form ? (
-      <StyledForm onSubmit={handleSubmit}>
+      <StyledForm onSubmit={e=>handleSubmit(e)}>
       
       <Input
           id="hability"
@@ -47,6 +59,12 @@ export default function CrearHabilidad() {
           placeholder="Nombre de la habilidad"
           value={form.hability}
           onChange={handleFormChange}
+        />
+        <Input
+        id="image"
+        name="image"
+        label="Imagen"
+        type="file"
         />
 
         <Input
@@ -59,7 +77,7 @@ export default function CrearHabilidad() {
         />
        
           <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center' type="submit">
-        Crear Empleado
+        Crear Habilidad
       </button>
         
     </StyledForm>) : (<div>Cargando....</div>)}
