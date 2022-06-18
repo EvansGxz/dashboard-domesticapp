15e7884429ad86806da219b481d80f494600814d
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-18 11:58:03 -0500

    Add calendar component

--- src/pages/Calendario.jsx
+++ src/pages/Calendario.jsx
@@ -1,28 +1,58 @@
 import styled from '@emotion/styled'
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Navegador } from '../components/Navegador'
 import { Sidebar } from '../components/Sidebar'
 import { deleteOrder, indexOrder } from '../services/order-details-services'
 import CrearOrder from './Order/createOrdr'
-import $ from 'jquery'
+import { Popdiv } from './pop'
+//Fullcalendar and Realted Plugins
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"; // needed
+import listPlugin from '@fullcalendar/list'; //For List View
 
 
 
 export const Calendario = () => {
-  $.DataTable = require('datatables.net')
-  const [categories, setCategories] = useState(null);
-  useEffect(() => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(true);
+  const [cal, setCal] = useState({});
+  const navigate = useNavigate();
+ useEffect(() => {
     indexOrder().then(setCategories)
-  }, [setCategories])
-  
+  }, [])
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+    setShow(!show);
+  }
+
   function handleDelete(id){
     deleteOrder(id).then(() => {
-      window.location.reload();
-     });}
-     if (categories){
-      $('#dataTable').DataTable()
-     }
+      indexOrder().then(setCategories)
+    });
+ }
+  const [categories, setCategories] = useState(null);
+ 
+    let details= []
+    if(categories){
+      categories.forEach(category =>{
+        details.push({title: category.address,
+                      id: category.id,
+                      date: category.start_date,
+                      start_date: category.start_date,
+                      workday: category.workday,
+                      supply_food: category.supply_food,
+                      service: category.category.category_name,
+                      region: category.category.region,
+                      employee: category.employee.full_name,
+                      employee_direction: category.employee.region,
+                      customer: category.customer.full_name
+                    })
+      })
+      console.log(details)
+    }
+  
   return (
     <>
       <Sidebar></Sidebar>
@@ -54,97 +84,63 @@ export const Calendario = () => {
           </div>
         </div>      
 
-          {
-            categories ? (
-              <>
-                <div class="container mx-auto">
-                  <div class="flex flex-col">
-                    <div class="w-full">
-                      <div class="p-4 border-b border-gray-200 shadow">
-                      
-                        <table id="dataTable" class="p-4">
-                          <thead class="bg-gray-50">
-                              <tr>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    ID
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Cliente
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Empleado
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Servcio
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Fecha
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Servcio
-                                  </th>
-                                  <th class="p-8 text-xs text-gray-500">
-                                    Activo
-                                  </th>
-                                  <th class="px-6 py-2 text-xs text-gray-500">
-                                      Edit
-                                  </th>
-                                  <th class="px-6 py-2 text-xs text-gray-500">
-                                      Delete
-                                  </th>
-                              </tr>
-                          </thead>
-                          <tbody class="bg-white">
-                            {categories.map((category, index) => {
-                            return (
-                              <>
-                              <tr key={index} class="whitespace-nowrap">
-                                                <td class="px-6 py-4 text-sm text-center text-gray-500">
-                                                {category.id}
-                                                </td>
-                                                <td class="px-6 py-4 text-center">
-                                                    <div class="text-sm text-gray-900">
-                                                    {category.customer.full_name}
-                                                    </div>
-                                                </td>
-                                                <td class="px-6 py-4 text-center">
-                                                    <div class="text-sm text-gray-500">{category.employee.full_name}</div>
-                                                </td>
-                                                <td class="px-6 py-4 text-sm text-center text-gray-500">
-                                                {category.category.category_name}
-                                                </td>
-                                                <td class="px-6 py-4 text-sm text-center text-gray-500">
-                                                {category.start_date}
-                                                </td>
-                                                <td class="px-6 py-4 text-sm text-center text-gray-500">
-                                                {category.workday}
-                                                </td>
-                                                {category.active ? (<td className="px-6 py-4 text-sm text-center text-gray-500">Si</td>) : (
-                                                <td className="px-6 py-4 text-sm text-center text-gray-500">No</td>
-                                                  )}
-                                                <td class="px-6 py-4 text-center">         
-                                                    <Link 
-                                                  className='px-4 py-1 text-sm text-white bg-blue-400 rounded'
-                                                  to={`/calendar/edit?id=${category.id}`}>Edit</Link>
-                                                </td>
-                                                <div id={category.id} onClick={()=>handleDelete(category.id)}>
-                                                <td class="px-6 py-4 text-center">
-                                                    <div href="#" class="px-4 py-1 text-sm text-white bg-red-400 rounded">Delete</div>
-                                                </td></div>
-                                            </tr>
-                                            </>)
-                          })}
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <h2>No se encontraron Clientes</h2>
-            )
-          } </div>  
+        <>
+     {
+      isOpen && <Popdiv
+      content={<>
+      <Box>
+      <Title>Detalles de servicio</Title></Box>
+      <Container>
+        <P><b>Dirección del servicio:</b> {cal.title}</P>
+        <P><b>Cliente: </b>{cal._def.extendedProps.customer}</P>
+        <P><b>Empleado: </b>{cal._def.extendedProps.employee}</P>
+        <P><b>País: </b>{cal._def.extendedProps.region}</P>
+        <P><b>Servicio: </b>{cal._def.extendedProps.service}</P>
+        <P><b>Fecha del servicio: </b>{cal._def.extendedProps.start_date}</P>
+        <P><b>Suplir alimentos: </b>{cal._def.extendedProps.supply_food}</P>
+        <P><b>Tipo de jornada: </b>{cal._def.extendedProps.workday}</P>
+        <ButtonContainer>
+        <Button onClick={()=>navigate(`/calendar/edit/?id=${cal._def.publicId}`)}>Editar Servicio</Button>
+        <Button onClick={()=>handleDelete(cal._def.publicId)}>Cancelar Servicio</Button>
+        </ButtonContainer>
+        </Container>
+      </>}
+      handleClose={togglePopup}
+    />
+    }
+    {
+      categories ? (
+        <>
+        {
+          show ? (
+            <>
+              <div className="maincontainer">
+              <FullCalendar
+                plugins={[ dayGridPlugin, interactionPlugin, listPlugin ]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                  left: 'prev,next today',
+                  center: 'title',
+                  right: 'dayGridMonth,dayGridWeek,dayGridDay,listWeek'
+                }}
+                eventClick={
+                  function(arg){
+                    setCal(arg.event);
+                    togglePopup()
+                  }
+                }
+                events={details}
+              />
+        </div>
+            </>
+          ) : null
+        }
+        </>
+      ) : null
+    }
+      
+    </>
+          </div>  
     </div>
     </>
   )
@@ -161,4 +157,45 @@ export const ContainerAll = styled.div`
   height: 50vh;
   padding-left: 18rem;
   padding-top: 5rem;
+`;
+
+export const Title = styled.p`
+  text-align: center;
+  margin: 1rem 0;
+  font-size: 2rem;
+  
+`;
+export const P = styled.p`
+  margin: 0.225rem 0;
+  font-size: 1rem;
+`;
+
+const Button = styled.button`
+  display: flex;
+  width: "fit-content";
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.75rem;
+  background-color: #0BBBEF;
+  border-radius: 10px;
+  color: #FFF;
+  border: none;
+  margin: 1rem auto;
+`;
+
+export const Box = styled.div`
+  width: 100%;
+  color: #FFF;
+  background-color: #0BBBEF;
+`;
+export const Container = styled.div`
+  width: 100%;
+  padding: 20px;
+`;
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin: 1rem auto;
+  padding: 20px;
 `;
