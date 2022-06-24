6476bc3fdb46ad24dee1f19c25b6381c5b53c3bf
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-24 16:23:53 -0500

    Add image update

--- src/pages/Servicios/editarServicio.jsx
+++ src/pages/Servicios/editarServicio.jsx
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 import { useEffect, useState } from "react";
-import { indexCategories, showCategory, updateCategory } from "../../services/categories-services";
+import { BASE_URI } from "../../Config";
+import {
+  indexCategories,
+  showCategory,
+  updateCategory,
+} from "../../services/categories-services";
 import { Input } from "../../styles/views/Login";
-
 
 const StyledForm = styled.form`
   display: flex;
@@ -22,77 +26,98 @@ const Container = styled.div`
   align-content: center;
 `;
 
-export default function EditarServicio({onStateChange, onInputChange}) {
+export default function EditarServicio({ onStateChange, onInputChange }) {
   const [form, setForm] = useState(null);
   const id = localStorage.getItem("ServID");
-  useEffect(() =>{
-  
-    showCategory(id).then((category) =>{
+  useEffect(() => {
+    showCategory(id).then((category) => {
+      
       setForm({
         category_name: category.category_name,
         price_col_complete: category.price_col_complete,
         price_col_half: category.price_col_half,
         price_spain: category.price_spain,
-      })
-    })
-  }, [id])
+        image: category.image,
+      });
+    });
+  }, [id]);
 
   function handleSubmit(event) {
     event.preventDefault();
-
-    updateCategory(form, id).then(()=>{
-      onInputChange(false);
-      indexCategories().then(onStateChange)
-    })
+    const data = new FormData();
+    
+    data.append("image", event.target.image.files[0]);
+    updateCategory(form, id)
+    .then(indexCategories().then(onStateChange))
+    .then(submitAPI(data, id))
+    onInputChange(false);
   }
-
+  function submitAPI(data, id) {
+    fetch(BASE_URI + `categories/${id}`, {
+      method: "PATCH",
+      body: data,
+    }).then(indexCategories().then(onStateChange))
+      .then((response) => response.json())
+      .catch((error) => console.log(error.message));
+  }
   function handleFormChange(event) {
-   
     const { name, value } = event.target;
     setForm({ ...form, [name]: value });
   }
 
   return (
     <Container>
-    {form ? (
-      <StyledForm onSubmit={handleSubmit}>
-      <Input
-        id="category_name"
-        label="Nombre de servicio"
-        type="text"
-        placeholder="Limpieza de hogar"
-        value={form.category_name}
-        onChange={handleFormChange}
-      />
-      <Input
-        id="price_col_complete"
-        label="Precio de servicio Colombia"
-        type="text"
-        placeholder="3000"
-        value={form.price_col_complete}
-        onChange={handleFormChange}
-      />
-      <Input
-        id="price_col_half"
-        label="Precio de servicio Colombia"
-        type="text"
-        placeholder="3000"
-        value={form.price_col_half}
-        onChange={handleFormChange}
-      />
-    
-      <Input
-        id="price_spain"
-        label="Precio de servicio Europa"
-        type="text"
-        placeholder="3000"
-        value={form.price_spain}
-        onChange={handleFormChange}
-      />
-      <button class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center' type="submit">
-        Actualizar Servicio
-      </button>
-    </StyledForm>) : (<div>Cargando....</div>)}
+      {form ? (
+        <StyledForm onSubmit={e=>handleSubmit(e)}>
+          <Input
+            id="category_name"
+            label="Nombre de servicio"
+            type="text"
+            placeholder="Limpieza de hogar"
+            value={form.category_name}
+            onChange={handleFormChange}
+          />
+          <Input
+            id="price_col_complete"
+            label="Precio de servicio Colombia"
+            type="text"
+            placeholder="3000"
+            value={form.price_col_complete}
+            onChange={handleFormChange}
+          />
+          <Input
+            id="price_col_half"
+            label="Precio de servicio Colombia"
+            type="text"
+            placeholder="3000"
+            value={form.price_col_half}
+            onChange={handleFormChange}
+          />
+
+          <Input
+            id="price_spain"
+            label="Precio de servicio Europa"
+            type="text"
+            placeholder="3000"
+            value={form.price_spain}
+            onChange={handleFormChange}
+          />
+          <Input
+            id="image" name="image"
+            value={form.image.files}
+            label="Imagen"
+            type="file"
+          />
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            type="submit"
+          >
+            Actualizar Servicio
+          </button>
+        </StyledForm>
+      ) : (
+        <div>Cargando....</div>
+      )}
     </Container>
   );
 }
