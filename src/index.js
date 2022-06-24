ec132b68e81f861e0f5a20309ffba830d62cd55e
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-23 19:45:24 -0500

    remove css

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './index.css';
 import './input.css';
-import '../public/css/main.css';
-import '../public/css/pop.css';
-import '../public/popdiv.css';
 import { hydrate, render } from "react-dom";
 import { AuthProvider } from "./context/auth-context";
 
