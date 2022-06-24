cbbbffb27a3340a17d5a1fb5227890de0097fa27
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-23 19:54:55 -0500

    fixes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import './index.css';
 import './input.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { hydrate, render } from "react-dom";
 import { AuthProvider } from "./context/auth-context";
 
@@ -27,3 +28,8 @@ if (rootElement.hasChildNodes()) {
     </BrowserRouter>
   , rootElement);
 }
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
